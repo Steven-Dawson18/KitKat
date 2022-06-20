08bd26a53f7a6d0e93326d5d3fd98b46aaf3a113
Steve <la14b@yahoo.co.uk>
2022-06-20 16:28:40 +0000

    Added code in views.py and template but unable to get working in time

--- catapp/views.py
+++ catapp/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
+from django.conf import settings
+import requests
+
+API_KEY = settings.API_KEY
 
 
 def index(request):
     """ A view to return the index page """
+    try:
+        baseurl = 'https://api.thecatapi.com/'
+        endpoint = 'v1/breeds'
+        token = '57833af1-b126-472a-be08-be42f992da0b'
 
-    template = 'index.html'
+        headers = {'x-api-key': token}
+        response = requests.get(
+            baseurl + endpoint, headers=headers).json()
 
-    return render(request, template)
+        print(response)
+
+        for item in response:
+            name = response[item]['name']
+            image = response[item]['image']
+            image_url = response[item]['image']['url']
+
+        context = {
+            'response': response,
+            'name': name,
+            'image': image,
+            'image_url': image_url,
+        }
+        return render(request, 'index.html', context)
+    except Exception:
+        return render(request, 'index.html')
